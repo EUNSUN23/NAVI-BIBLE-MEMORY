a844b06e5ca1f80e40f5d7338154530d15bcce47
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-06-25 21:27:28 +0900

    feat: BibleVersionSelect 테스크 추가

--- src/features/bibleVersionSelect/index.test.tsx
+++ src/features/bibleVersionSelect/index.test.tsx
@@ -1,3 +1,44 @@
-import { test } from 'vitest';
+import { userEvent } from '@testing-library/user-event';
+import BibleVersionSelect from '@features/bibleVersionSelect/index';
+import { render } from '@/lib/test/testUtils/render';
+import waitForElementToBeRemovedIfExist from '@/lib/test/testUtils/waitForElementToBeRemovedIfExist';
+import { screen, waitFor, within } from '@testing-library/react';
+import { expect } from 'vitest';
+import { BIBLE_VERSIONS } from '@/mock/mockData';
 
-test('BibleVersionSelect listbox renders bible version options', () => {});
+describe('BibleVersionSelect test', () => {
+  beforeAll(() => {
+    vi.mock('@/store/bibleVersionStore', async () => {
+      return await vi.importActual('@/store/bibleVersionStore');
+    });
+  });
+
+  test('renders bible version combobox after data loading finishes', async () => {
+    const user = userEvent.setup();
+    render(<BibleVersionSelect />);
+
+    await waitForElementToBeRemovedIfExist(
+      screen.queryByTestId('bibleVersionSelect-loader'),
+    );
+
+    const combobox = screen.getByRole('combobox', { name: '성경버전' });
+
+    await user.click(combobox);
+
+    const comboboxButton = await screen.findByRole('button', {
+      expanded: false,
+    });
+
+    await user.click(comboboxButton);
+
+    const listbox = await screen.findByRole('listbox');
+
+    await waitFor(() => {
+      BIBLE_VERSIONS.forEach(v => {
+        expect(
+          within(listbox).queryByRole('option', { name: v.name }),
+        ).not.toBeNull();
+      });
+    });
+  });
+});
