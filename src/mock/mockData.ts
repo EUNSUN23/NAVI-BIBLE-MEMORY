58a8030dcf6c7784c7fb1b47b2a9bcd4338131e4
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-06-27 00:17:16 +0900

    fix: CARD_HIDE_OPTIONS 데이터 수정

--- src/mock/mockData.ts
+++ src/mock/mockData.ts
@@ -1394,8 +1394,8 @@ export const BIBLE_VERSIONS = [
 ] as const;
 
 export const CARD_HIDE_OPTIONS = [
-  { code: 'HIDE_000', name: '없음' },
-  { code: 'HIDE_001', name: '장절' },
-  { code: 'HIDE_002', name: '제목' },
-  { code: 'HIDE_003', name: '내용' },
+  { code: 'HIDE_001', name: '없음' },
+  { code: 'HIDE_002', name: '장절' },
+  { code: 'HIDE_003', name: '제목' },
+  { code: 'HIDE_004', name: '내용' },
 ] as const;
