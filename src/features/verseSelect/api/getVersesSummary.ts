66ff136bed801584c026f0f4762ee159ceee230d
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-09-17 17:27:16 +0900

    fix: 말씀요약정보 조회시 시리즈 내 말씀순서로 정렬되도록 수정

--- src/features/verseSelect/api/getVersesSummary.ts
+++ src/features/verseSelect/api/getVersesSummary.ts
@@ -10,7 +10,7 @@ export const getVersesSummary = async (seriesCode: SeriesCode) => {
       'idx,card_num,series_code,category,theme,chapter,verse1,verse2,bible_code(bible_name)',
     )
     .eq('series_code', seriesCode)
-    .order('idx', { ascending: true });
+    .order('card_num', { ascending: true });
 
   return supabaseResponseHandler(res);
 };
