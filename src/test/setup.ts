f78ede500a10396c3400294865ed7148991c12bf
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-12-17 18:48:22 +0900

    chore: 테스트 범용 setup, teardown코드 vite.setup.ts안으로 이동

--- src/test/setup.ts
+++
@@ -1,8 +0,0 @@
-import { afterAll, beforeAll, afterEach } from 'vitest';
-import { server } from '@/msw/node';
-
-beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
-
-afterAll(() => server.close());
-
-afterEach(() => server.resetHandlers());
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,5 +1,21 @@
 import '@testing-library/jest-dom/vitest';
-import { vi } from 'vitest';
+import { afterAll, afterEach, beforeAll, vi } from 'vitest';
+import { server } from '@/msw/node';
+
+beforeAll(() => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  server.listen({ onUnhandledRequest: 'error' });
+});
+
+afterAll(() => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  server.close();
+});
+
+afterEach(() => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  server.resetHandlers();
+});
 
 const ResizeObserverMock = vi.fn(() => ({
   observe: vi.fn(),
