22ba10b4b2b5e9b9dcc2b0b0e0eed9a93385e537
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-12-21 00:00:37 +0900

    chore: vitest.config.ts 분리, tsconfig app/eslint/vitest 분리

---
+++ vitest.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tsconfigPaths({
+      projects: ['./tsconfig.vitest.json'],
+    }),
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.ts',
+    bail: 1,
+    silent: true,
+    retry: 2,
+  },
+});
