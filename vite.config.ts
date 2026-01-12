c0241e5108245cb62a1b8687d3a31476ecc77c6f
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2026-01-12 17:55:00 +0900

    chore: 번들 분석 위해 rollup-plugins-visualizer 셋업

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, type PluginOption } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths({ projects: ['./tsconfig.app.json'] })],
+  plugins: [
+    react(),
+    tsconfigPaths({ projects: ['./tsconfig.app.json'] }),
+    visualizer({
+      filename: './dist/report.html',
+      template: 'treemap',
+      gzipSize: true,
+      brotliSize: true,
+    }) as PluginOption,
+  ],
+  build: {
+    sourcemap: true,
+  },
 });
