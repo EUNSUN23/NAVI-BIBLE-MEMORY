5e43a0bb3d4c3cfc990412aa10de5ec6bd7dbe2c
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2026-01-16 18:58:23 +0900

    refactor: queryClient shared/api로 이동

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,18 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { App } from './app';
 import { BrowserRouter } from 'react-router-dom';
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 60 * 24,
-      gcTime: 1000 * 60 * 60 * 24,
-    },
-  },
-});
+import { queryClient } from '@shared/api/query-client';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
